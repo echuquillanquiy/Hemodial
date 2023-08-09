ca9ef4f77da4b0e57853ecb9f736ac2ceb2dba01
echuquillanquiy <edward.axl3@gmail.com>
2023-08-09 17:17:28 -0500

    empezando nuevo proyecto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('dni')->unique()->nullable();
             $table->string('cmp')->unique()->nullable();
             $table->string('rne')->unique()->nullable();
